206dcb4c3eacd15f6844c4c64c21c7907d72c76a
Kas Barber <kaseanbarber02@gmail.com>
2021-09-28 16:06:43 -0400

    added comments

--- Program.cs
+++ Program.cs
@@ -17,12 +17,14 @@ namespace ConsoleApp6
             {
                 Console.WriteLine("Hi "+name+", please enter an integer between 1 and 100");
                 int numberGiven = int.Parse(Console.ReadLine());
+                //verifying is the number between 1 and 100
                 if (!(numberGiven >= 1 && numberGiven <= 100))
                 {
                     Console.WriteLine("Please try again "+name+", number was not in the acceptable range.");
                 }
                 else
                 {
+                    //displaying a message depending on the number given
                     if (numberGiven % 2 == 1)
                     {
                         Console.WriteLine(numberGiven + " is odd.");
@@ -41,8 +43,10 @@ namespace ConsoleApp6
                     }
 
                 }
+                
                 Console.WriteLine("Continue? Type n to quit or press any key to try again.");
                 string repeatAnswer = Console.ReadLine();
+                //ends the loop only if n is typed
                 if (repeatAnswer == "n")
                 {
                     keepGoing = false;
